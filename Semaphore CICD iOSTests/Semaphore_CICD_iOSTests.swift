83b7a0c468b348b948989800115ef79e52e658ed
amarildo <vmarildo@gmail.com>
2019-09-17 08:44:26 -0300

    First commit

---
+++ Semaphore CICD iOSTests/Semaphore_CICD_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Semaphore_CICD_iOSTests.swift
+//  Semaphore CICD iOSTests
+//
+//  Created by Amarildo Lucas on 17/09/19.
+//  Copyright © 2019 Amarildo Lucas. All rights reserved.
+//
+
+import XCTest
+@testable import Semaphore_CICD_iOS
+
+class Semaphore_CICD_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
